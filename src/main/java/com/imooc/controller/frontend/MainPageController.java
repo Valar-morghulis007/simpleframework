2aa2de5068544b0d87a94b35a5af2c3b69e7ef4b
Valar-morghulis007 <1261644617@qq.com>
2020-10-05 22:48:55 +0800

    新增了MVC

--- src/main/java/com/imooc/controller/frontend/MainPageController.java
+++ src/main/java/com/imooc/controller/frontend/MainPageController.java
@@ -6,19 +6,27 @@ import com.imooc.service.combine.HeadLineShopCategoryCombineService;
 import lombok.Getter;
 import org.simpleframework.core.annotation.Controller;
 import org.simpleframework.inject.annotation.Autowired;
+import org.simpleframework.mvc.annotation.RequestMapping;
+import org.simpleframework.mvc.type.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
-@Getter
+//@Getter
+@RequestMapping(value = "/main")
 public class MainPageController {
+
     @Autowired("HeadLineShopCategoryCombineServiceImpl")
     private HeadLineShopCategoryCombineService headLineShopCategoryCombineService;
 
     public Result<MainPageInfoDTO> getMainPageInfo(HttpServletRequest req,
                                                    HttpServletResponse resp) {
-
         return headLineShopCategoryCombineService.getMainPageInfo();
+    }
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public void throwException(){
+       throw new RuntimeException("抛出异常测试");
     }
 }
